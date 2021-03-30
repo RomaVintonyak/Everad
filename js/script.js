00dd75581c00383428e98741c40057e3f21a853e
RomaVintonyak <19roman23@gmail.com>
2021-03-30 16:54:46 +0300

    create slider section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 jQuery(document).ready(function(){
   "use script";
-  
+  //main slider
+  var mainSlider = $("#mainSlider");
+  mainSlider.slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 });
